47be0a189bcf842975ff6bb9ad6deeea70fa8099
ohmydearcheesus <ohmydearcheesus@gmail.com>
2020-03-11 16:32:45 -0700

    setting up header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <div>Byleth's Class Planner</div>
+        {/* <p>
           Edit <code>var = "meowoof"</code> and save to reload.
         </p>
         <a
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
---
+++ my-app/src/table/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Planner extends Component {
+    render () {
+        return (
+            <div className="plannerbook">
+                <div></div>
+            </div>
+        )
+    }
+}
